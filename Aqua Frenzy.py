89439c849268dd027419b340d446bbfac9ea8a22
mha66 <mharegaily@gmail.com>
2025-04-21 15:47:30 +0200

    add shark

--- Aqua Frenzy.py
+++ Aqua Frenzy.py
@@ -7,7 +7,6 @@ from pygame.locals import *
 
 #(2/size)*x - 1
 
-    
 def triangle(x1, y1, x2, y2, x3, y3, r, g, b):
     glBegin(GL_TRIANGLES)
     glColor3f(r/255, g/255, b/255)
@@ -27,6 +26,7 @@ def rectangle_gradient(x1, y1, x2, y2, r1, g1, b1, r2, g2, b2):
     glVertex2f(x2, y1)
     glEnd()
 
+
 def circle(xc, yc, radius, num_segments, r, g, b):
     glBegin(GL_TRIANGLE_FAN)
     glColor3f(r/255, g/255, b/255)
@@ -42,6 +42,18 @@ def ellipse(xc, yc, radius_x, radius_y, num_segments, r, g, b):
     glColor3f(r/255, g/255, b/255)
     for i in range(num_segments):
         angle = 2*mm.pi*i/num_segments
+        x = xc + radius_x * mm.cos(angle)
+        y = yc + radius_y * mm.sin(angle)
+        glVertex2f(x, y)
+    glEnd()
+
+def ellipse_gradient(xc, yc, radius_x, radius_y, num_segments, r1, g1, b1, r2, g2, b2, ratio = 0.5):
+    glBegin(GL_TRIANGLE_FAN)
+    glColor3f(r1/255, g1/255, b1/255)
+    for i in range(num_segments):
+        angle = 2*mm.pi*i/num_segments
+        if i >= num_segments * ratio:
+             glColor3f(r2/255, g2/255, b2/255)
         x = xc + radius_x * mm.cos(angle)
         y = yc + radius_y * mm.sin(angle)
         glVertex2f(x, y)
@@ -76,6 +88,30 @@ def fish(x, y, r, g, b, size=1.0, reverse=False):
         triangle(-0.09, 0.2, -0.1, 0.1, 0.1, 0.1, r, g, b)
         glPopMatrix()
 
+def shark(x, y, size=1.0, reverse=False):
+        upper_color = (151, 156, 168)
+        lower_color = (180, 186, 191)
+        glPushMatrix()
+        glScale(size, size, 0)
+        glTranslate(x, y, 0)
+        if reverse:
+            glRotate(180, 0, 1, 0)
+        #snout
+        triangle(-0.4, 0.08, -0.4, -0.08, -0.55, 0.02, *upper_color)
+        #tail fins
+        triangle(0.46, 0, 0.9, 0.4, 0.65, 0, *upper_color)
+        triangle(0.46, 0, 0.7, -0.35, 0.65, 0, *upper_color)
+        #upper fin
+        triangle(-0.35, -0.04, 0.1, -0.04, 0, 0.35, *upper_color)
+        #lower fins
+        triangle(0.15, -0.1, 0.32, -0.1, 0.3, -0.17, *lower_color)
+        triangle(-0.25, -0.1, 0, -0.1, 0.05, -0.2, *lower_color)
+        #body
+        ellipse_gradient(0, 0, 0.5, 0.13, 180, *upper_color, *lower_color, 0.55)
+        #eye
+        circle(-0.4, 0.03, 0.015, 180, 0, 0, 0)
+        glPopMatrix()
+
 
 def main():
     pg.init()
@@ -84,7 +120,6 @@ def main():
     pg.display.set_mode(display, DOUBLEBUF|OPENGL)
 
     glOrtho(-1, 1, -1, 1, -1, 1)
-    #glClearColor(65/255, 142/255, 188/255, 1) 
     glDisable(GL_DEPTH_TEST)  
     
     while True:
@@ -94,13 +129,13 @@ def main():
                 return
         
         glClear(GL_COLOR_BUFFER_BIT)
+        #background
         rectangle_gradient(-1, -0.7, 1, 1, 23, 20, 188, 65, 142, 188)
         rectangle_gradient(-1, -1, 1, -0.7, 187, 134, 23, 240, 180, 58)
-        # triangle(*normalize(471, 397, 148, 159, 156, 203, 182, 203), 1, 0, 0)
-        # triangle(*normalize(471, 397, 148, 245, 156, 203, 182, 203), 1, 0, 0)
-        fish(0.4, 0.2, 255, 0, 0, 0.6, True)
+
+        shark(0, 0)
+        fish(0.4, 0.8, 255, 0, 0, 0.6, True)
         
-        # triangle(-0.2, -0.2, 0.2, -0.2, 0, 0.2, 1,0,0)
         pg.display.flip()
         pg.time.wait(10)
 
